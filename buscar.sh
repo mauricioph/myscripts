9f2504ce548f9e75d691bedb6cfea3fb19a107e4
mauricioph <jesusmlmph@gmail.com>
2019-04-15 16:09:58 +0100

    Update buscar.sh

--- buscar.sh
+++ buscar.sh
@@ -3,8 +3,8 @@ mes=$(date +%B)
 ans="/home/mauricio/Podcasts/My Own/pt/${mes}"
 
 if [ -f /tmp/urls ]
-        then echo "There is a list of links here /tmp/usrls"
-        else echo "updating the list, you can get the links at /tmp/urls"
+        then echo "The links are here /tmp/usrls"
+        else echo "Updating the links at /tmp/urls"
                 for i in $(curl https://soundcloud.com/bispomacedo | grep url | grep itemprop | cut -d "=" -f 4 | cut -d ">" -f 1)
                         do echo https://soundcloud.com$(echo ${i} | sed 's/"//g') >> /tmp/urls
                 done
@@ -13,13 +13,17 @@ fi
 		then mkdir -p "${ans}"
 	fi
 
-echo "Enter the link"
-read link
-echo "Enter the name"
-read dest
-
-wget "${link}" -O "${ans}/${dest}.mp3" 
-
-		if [ "${1}" = "-p" ]
-			then vlc "${ans}/${dest}.mp3"
+function linkage(){
+	for i in $(cat /tmp/urls | sed -n 2,10p);
+	do dest=$(youtube-dl --get-title "${i}" | cut -d "-" -f 1)
+	orig=$(youtube-dl --get-title "${i}" | cut -d "-" -f 2 | sed 's/\//\./g' | awk '{print $1}')
+	saida=$( echo "${ans}/${orig}-${dest}.mp3" | sed 's/\ .mp3/\.mp3/')
+		if [ -f "${saida}" ]
+			then echo "File ${saida} is ready"
+			else youtube-dl "${i}" -o "${saida}"
 		fi
+		sleep 3;
+#	clear
+	done
+}
+linkage
