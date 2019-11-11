8b2af5984bdbcb6030f3183e8e74e86ded7e2386
mauricioph <jesusmlmph@gmail.com>
2019-11-11 00:36:06 +0000

    Update transcription.sh
    use /tmp and add some comments.

--- Translation/transcription.sh
+++ Translation/transcription.sh
@@ -3,20 +3,24 @@ if [ -z ${1} ]
 	then echo "Usage: $0 audio-file"
 fi
 
-audio-file = "${1}"
+# Audio file is passed on command line as argument, total lenght of audio is divided by 10 seconds blocks
+# and saved as transcript.mp3 on the /tmp folder, python script is called to write the words on a file
 
-b=$(cat ${audio-file} | wc -l)
+audiofile="${1}"
+b=$(($(mp3info -p "%S" ${audiofile}) / 10))
 a=0
+
 while [ "${a}" != "${b}" ]
- do ffmpeg -i "${audio-file}" -ss ${a} -t 10 -c copy -y /tmp/transcript-${a}.mp3
- cp /tmp/transcript-${a}.mp3 /tmp/transcript.mp3
+ do ffmpeg -i "${audiofile}" -ss ${a} -t 10 -c copy -y /tmp/transcript-${a}.mp3
+ mv /tmp/transcript-${a}.mp3 /tmp/transcript.mp3
  python3 /usr/local/bin/transcribe.py
  let a=$(($a + 10))
 done
 
-leng=$(cat /tmp/treino.txt | wc -l)
+# Text file with the words is translated to english. Translation is done line by line.
+leng=$(cat /tmp/BishopMacedo.txt | wc -l)
 a=1
-while [ "${a}" != "${leng}" ]
-do translate -d pt -s en "$(cat /tmp/treino.txt | sed -n ${a}p)" >> /tmp/traducao.txt
+while [ ${a} != ${leng} ]
+do translate -d en -s pt "$(cat /tmp/BishopMacedo.txt | sed -n ${a}p)" >> traducao.txt
 let a=$(($a + 1))
 done
