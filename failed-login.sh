2ca81aba66f07bcc96a4085c6d6f844740091144
mauricioph <jesusmlmph@gmail.com>
2019-12-03 13:32:37 +0000

    Update failed-login.sh
    Add telegram-send support

--- failed-login.sh
+++ failed-login.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Record a 10 second video of whoever is trying to login on my laptop and is guessing the password
+# It should be called by PAM and send the video via Telegram.
+# It requires telegram-send
+# https://pypi.org/project/telegram-send/#usage
+
 if [ ! -d /tmp/failed-login ]
 then mkdir -p /tmp/failed-login
 fi
@@ -7,3 +12,4 @@ video="movie-$(date +%d-%m-%Y_%H-%M)"
 ffmpeg -i /tmp/failed-login/${video}.avi -c:v libx264 -profile:v high -preset:v ultrafast -c:a aac -strict -2 -y /tmp/failed-login/${video}.mp4
 rm -f /tmp/failed-login/${video}.avi
 chmod 777 /tmp/failed-login -R
+telegram-send --caption "Someone is trying to login on your macbook-pro" --file /tmp/failed-login/${video}.mp4
