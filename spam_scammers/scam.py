65888ddf38048807954efb88d622bc89c4d999af
mauricioph <jesusmlmph@gmail.com>
2019-12-10 19:19:01 +0000

    Add files via upload

---
+++ spam_scammers/scam.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import random
+import string
+import json
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
+
+providers = [ "gmail.com", "yahoo.com", "mail.com", "hotmail.com", "live.com", "icloud.com", "me.com"]
+names = json.loads(open('names.json').read())
+
+for name in names:
+
+	name_extra = ''.join(random.choice(string.digits))
+	name_extra_tw = ''.join(random.choice(string.digits))
+	name_extra_tr = ''.join(random.choice(string.digits))
+	name_extra_fr = ''.join(random.choice(string.digits))
+	username = name.lower() + name_extra + name_extra_tw + name_extra_tr + name_extra_fr + '@' + random.choice(providers)
+	password = ''.join(random.choice(chars) for i in range(8))
+
+	requests.post(url, allow_redirects=False, data={
+		'auid2yjauysd2uasdasdasd': username,
+		'kjauysd6sAJSDhyui2yasd': password
+	})
+
+	print 'sending username %s and password %s' % (username, password)
