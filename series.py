d5e7e8388d55136112d0fe5a4e2d8c3d15c6b5e2
mauricioph <jesusmlmph@gmail.com>
2019-12-13 08:23:26 +0000

    Update series.py

--- series.py
+++ series.py
@@ -4,16 +4,16 @@ import os
 import random
 import subprocess
 
-folder_destination_path = input("Where are the files?\n")
-serie = input("What is the serie Name?\n")
-season = input("Which season is that?\n")
-network = input("Which Network?\n")
-year = input("What year?\n")
+folder_destination_path = raw_input("Where are the files?\n")
+serie = raw_input("What is the serie Name?\n")
+season = raw_input("Which season is that?\n")
+network = raw_input("Which Network?\n")
+year = raw_input("What year?\n")
 
 print ('Checking \nFolder = '+ folder_destination_path + '\nSerie = '+ serie + '\nSeason = ' + season + '\nNetwork = ' + network + '\nYear = '+ year)
 
 def ffmpeg():
- subprocess.call(['ffmpeg -i '+ new_name + ' -pix_fmt yuv444p10le -s 1920x1080 -threads 2 -c:v libx265 -maxrate 7M -bufsize 14M -preset ultrafast -profile:v:0 main444-10 -level:v:0 4.1 -crf 23 -x265-params subme=0:me_range=4:rc_lookahead=30:me=dia:no_chroma_me:8x8dct=0:partitions=none -g:v:0 72 -keyint_min:v:0 72 -sc_threshold:v:0 0 -metadata title="'+ serie +'" -metadata network="' + network +'" -metadata year="'+ year +'" -metadata comment="'+ series +' from '+ network +'" -c:a ac3 -b:a 192k -filter_complex "[0:a]join=inputs=1:channel_layout=5.1:map=0.0-FL|0.0-FR|0.0-FC|0.0-LFE|0.0-BL|0.0-BR[a]" -map 0:v:0 -map [a] -y '+ src], shell=True)
+ subprocess.call(['ffmpeg -i '+ new_name + ' -pix_fmt yuv444p10le -s 1920x1080 -threads 2 -c:v libx265 -maxrate 7M -bufsize 14M -preset ultrafast -profile:v:0 main444-10 -level:v:0 4.1 -crf 23 -x265-params subme=0:me_range=4:rc_lookahead=30:me=dia:no_chroma_me:8x8dct=0:partitions=none -g:v:0 72 -keyint_min:v:0 72 -sc_threshold:v:0 0 -metadata title="'+ serie +'" -metadata network="' + network +'" -metadata year="'+ year +'" -metadata comment="'+ serie +' from '+ network +'" -c:a ac3 -b:a 192k -filter_complex "[0:a]join=inputs=1:channel_layout=5.1:map=0.0-FL|0.0-FR|0.0-FC|0.0-LFE|0.0-BL|0.0-BR[a]" -map 0:v:0 -map [a] -y '+ src], shell=True)
 
 for x in range(10, 81):
  number = random.randint(1, 500)
