4fed4c743fe3da83f8c78e681811fed28a8c8d3f
mauricioph <jesusmlmph@gmail.com>
2020-04-06 13:49:06 +0100

    Cleaning the code
    Removing some ifs here and there.

--- makemkv.py
+++ makemkv.py
@@ -1,6 +1,7 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import subprocess
+import sys
 import os.path
 from os import path
 
@@ -9,31 +10,26 @@ def is_tool(name):
     from shutil import which
     return which(name) is not None
 
-ffmpeg = is_tool('ffmpeg')
-lsdvd = is_tool('lsdvd')
-mplayer = is_tool('mplayer')
-mkvtoolnix = is_tool('mkvtoolnix-gui')
+pgm = ['ffmpeg', 'lsdvd', 'mplayer', 'mkvmerge']
 
-if ffmpeg is False:
-    subprocess.call('sudo apt install ffmpeg', shell=True)
-
-if lsdvd is False:
-    subprocess.call('sudo apt install lsdvd', shell=True)
-
-if mplayer is False:
-    subprocess.call('sudo apt install mplayer', shell=True)
-
-if mkvtoolnix is False:
-    subprocess.call('sudo apt install mkvtoolnix-gui', shell=True)
+for programs in pgm:
+    checkpgm = is_tool(programs)
+    print ('The program %s is installed.' %(programs))
+    if checkpgm == False:
+        if programs == 'mkvmerge':
+            programs == 'mkvtoolnix'
+        subprocess.call('sudo apt install %s' %(programs), shell=True)
 
 # Check if the track was passed as an argumment, if not it will get the longest track
-# I am stucked here.
+if len(sys.argv) == 2:
+    track = sys.argv[1]
+else:
+    track = str(subprocess.check_output("lsdvd | grep Longest | cut -d "":"" -f 2 | awk '{print $1}'", shell=True))
+    track = track[2:-3]
 
 # Create the folder into Videos folder if it does not exit
 disctitle = str(subprocess.check_output("lsdvd | grep Disc | cut -d "":"" -f 2 | awk '{print $1}'", shell=True))
 disctitle = disctitle[2:-3]
-track = str(subprocess.check_output("lsdvd | grep Longest | cut -d "":"" -f 2 | awk '{print $1}'", shell=True))
-track = track[2:-3]
 folder = str.format(os.path.expanduser(os.getenv('HOME'))+"/Videos/"+disctitle)
 output = str.format(folder+"/title_t"+track+".vob")
 finalfile = output.replace(".vob", ".mkv")
@@ -41,9 +37,7 @@ finalfile = output.replace(".vob", ".mkv")
 if not path.exists(folder):
   subprocess.call('mkdir -p '+folder, shell=True)
 
-
 # Extract the chapters from the DVD and save the file in te right format
-
 capitulos = subprocess.check_output("mplayer -identify -frames 1 dvd://"+track+" 2>/dev/null | grep CHAPTERS: | sed 's/CHAPTERS: //' | sed 's/,/ /g'", shell=True)
 capitulos = capitulos.decode('utf-8')
 capitulos = capitulos.split(" ")
@@ -59,14 +53,18 @@ for i in  range(tot - 1):
 
 # Do the ripping of the file in vob first and then add the chapters on the mkv file
 if not path.exists(output):
-    subprocess.call("mplayer dvd://"+track+" -vc null -dumpstream -dumpfile "+output, shell=True)
+    subprocess.call("mplayer dvd://%s -vc null -dumpstream -dumpfile %s" %(track, output), shell=True)
 else:
-    print ("The output file "+output+" already exists, delete it. Not overwritting")
+    print ("The output file %s already exists, delete it. Not overwritting" %(output))
 
 if not path.exists(finalfile):
     subprocess.call("mkvmerge -o "+finalfile+" --chapters /tmp/chapter.txt "+output, shell=True)
 
+# This should check if the previous if worked, do not merge as if and elif it will not work.
+# This if needs to run after the other if and not together.
 if path.exists(finalfile):
     subprocess.call("rm -f "+output, shell=True)
     subprocess.call("rm -f /tmp/chapter.txt", shell=True)
     print ("File saved at "+finalfile)
+
+subprocess.call("eject", shell=True)
