aa21af69d6430026e9b6820067c10d40d4e05fd7
mauricioph <jesusmlmph@gmail.com>
2020-04-07 11:47:44 +0100

    Add files via upload
    Backup based on hard drive ID

---
+++ backup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import subprocess
+
+# THIS FILE HAS NOT BEEN TESTED YET, JUST CREATED... NOT SAFE TO DO USE IF YOU DO NOT KNOW WHAT TO EDIT
+
+hardriveuuid = "ffe7a7b8-daf8-45bd-86b0-d3a57acb99ea"
+device = subprocess.check_output("blkid | grep %s | cut -d "" "" -f 1 | sed 's/\://'" %(hardriveuuid), shell=True)
+pointmount = subprocess.call("mount | grep %s | cut -d "" "" -f 3" %(device), shell=True)
+
+if not device:
+    print ("The device is not connected, aborting backup. Please make sure it is plugged and the cable is working.")
+    exit (1)
+
+if not pointmount:
+    print ("The hard drive is not mounted yet, mounting it now on /mnt")
+    location = subprocess.call("mount | grep /mnt | cut -d "" "" -f 1", shell=True)
+    if not location:
+        print ("########## ERROR ############\n/mnt is mounted, but it is not your hard drive. umount it first")
+        exit (1)
+
+subprocess.call("mount %s /mnt" %(device), shell=True)
+pointmount = "/mnt"
+print ("rsync -havz --exclude=/mnt --exclude=/dev/ --exclude=/tmp --exclude=/proc --exclude=/sys / "+pointmount)
+
+# subprocess.call("rsync -havz --exclude=/mnt --exclude=/dev/ --exclude=/tmp --exclude=/proc --exclude=/sys / "+pointmount, shell=True)
