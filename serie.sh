66853c84c1334f07168a3d5c827adc918753fe75
mauricioph <jesusmlmph@gmail.com>
2020-05-25 23:14:02 +0100

    Update serie.sh

--- serie.sh
+++ serie.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 #
-#
-# Script to add metadata on Jesus  series
+# Script to add metadata on the episodes file of series downloaded from Telegram.
+
 locations="$HOME/.local/share/TelegramDesktop/tdata/tdld/"
 echo "Qual e a serie?"
 read serie
 echo "Preparando ${serie}"
+
+echo "Do you know the year for this season?"
+read ano
+
+echo "What is the network that aired this serie?"
+read network
+
 cd ${locations}
 
+# Transfer from computer to media server via ssh
 trsnf(){
 for i in /tmp/${serie}-S01x*
 do scp "${i}" n40l.local.home:/mnt/Seagate/plex/Series/${serie}/
-rm -f "${i}"
 done
 }
 
+# add metadata
 conv(){
-ffmpeg -i "${i}" -c:v copy -c:a copy -async 1 -metadata copyright="${year} Record TV" -metadata title="${serie}" -metadata episode_id="${episode_id}" -metadata show="${serie}" -metadata network="Record TV" -metadata year="${year}" -metadata comment="Novela da Rede Record" -y "${saida}"
-rm -f "${i}"
+ffmpeg -i "${i}" -c:v copy -c:a copy -async 1 -metadata copyright="${year} ${network}" -metadata title="${serie}" -metadata episode_id="${episode_id}" -metadata show="${serie}" -metadata network="${network}" -metadata year="${year}" -metadata comment="${serie} from ${network}" -y "${saida}"
 }
+
 
 for i in ${serie}*
 do "checking file ${i}"
@@ -32,13 +40,14 @@ fi
 episode_id=$(echo "${i}" | cut -d "." -f 3)
 episode_id=$(echo ${episode_id} | sed 's/S01E//g')
 
-	if [ $episode_id -eq $episode_id ]
+	if [[ $episode_id =~ ^-?[0-9]+$ ]]
 	then echo "episode checked"
 	else echo "${i} is not in the right format for episode detection"
 	fi
 year=$(date +%Y)
 saida="/tmp/${serie}-S01x${episode_id}.mp4"
 conv
+# rm "${i}"
 done
 echo "Do you want to transfer the files now?"
 read trx;
@@ -47,6 +56,6 @@ case $trx in
 	;;
 	yes) trsnf
 	;;
-	*) echo "finished, files were kept in /tmp\n Transfer them before reboot or they will be automatically deleted."
+	*) echo "finished"
 	;;
 esac
