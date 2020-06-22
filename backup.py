8535d146fa53cf5af4b45e492f0f476f71663eee
mauricioph <jesusmlmph@gmail.com>
2020-06-22 20:43:28 +0100

    New hard drive UUI

--- backup.py
+++ backup.py
@@ -4,7 +4,7 @@ import subprocess
 
 # THIS FILE HAS NOT BEEN TESTED YET, JUST CREATED... NOT SAFE TO DO USE IF YOU DO NOT KNOW WHAT TO EDIT
 
-hardriveuuid = "ffe7a7b8-daf8-45bd-86b0-d3a57acb99ea"
+hardriveuuid = "2ea8b5cd-95e3-42fb-a326-5e7d268fb581"
 device = subprocess.check_output("blkid | grep %s | cut -d "" "" -f 1 | sed 's/\://'" %(hardriveuuid), shell=True)
 pointmount = subprocess.call("mount | grep %s | cut -d "" "" -f 3" %(device), shell=True)
 
