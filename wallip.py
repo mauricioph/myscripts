43a9233146066ec65c7b54873bc3d4418227635f
mauricioph <jesusmlmph@gmail.com>
2023-05-19 01:35:51 +0100

    Added the function that updates the quotes using text from the website biblegateway

--- wallip.py
+++ wallip.py
@@ -8,15 +8,37 @@ import subprocess
 import textwrap
 import configparser
 import html
+import requests
+from bs4 import BeautifulSoup
+
+def update_quote_of_the_day():
+    # Send a GET request to the Bible Gateway homepage
+    response = requests.get("https://www.biblegateway.com/")
+
+    # Create a BeautifulSoup object to parse the HTML content
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find the div with id "verse-text" and extract the text
+    verse_text_div = soup.find("div", id="verse-text")
+    quote_of_the_day = verse_text_div.text.strip()
+
+    # Update the config file with the new quote
+    config = configparser.ConfigParser()
+    config_file = "~/.config/wallip.conf"  # Update with the correct path to the config file
+    config_file = config_file.replace("~", os.path.expanduser("~"))
+    config.read(config_file)
+    config.set("QUOTE", "text", quote_of_the_day)
+    with open(config_file, "w") as f:
+        config.write(f)
+
+# Call the function to update the quote of the day
+update_quote_of_the_day()
 
 # Read the config file
 config = configparser.ConfigParser()
 config_file = os.path.expanduser('~/.config/wallip.conf')
 config.read(config_file)
 
-# config.read(os.path.expanduser('/home/mauricio/.config/wallip.conf'))
-# print (config)
-# Extract the quote from the config file
 quote = html.unescape(config['QUOTE']['text'])
 
 # Set the folder containing the photos
@@ -43,30 +65,33 @@ while True:
     font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
     text = "Offline"
 
-    # Check if the computer is online
     try:
         s = socket.create_connection(("www.google.com", 80), 2)
         text = "Online - IP: " + s.getsockname()[0]
     except:
         pass
 
-    text_width, text_height = draw.textsize(text, font)
-    # Draw the text with a shadow
+    text_bbox = draw.textbbox((0, 0), text, font=font)
+    text_width = text_bbox[2] - text_bbox[0]
+    text_height = text_bbox[3] - text_bbox[1]
+
     shadow_color = (0, 0, 0)  # black
     shadow_offset = (2, 2)   # 2 pixels to the right and 2 pixels down
 
     draw.text(((1920 - text_width) / 2 + shadow_offset[0], shadow_offset[1]), text, font=font, fill=shadow_color)
     draw.text(((1920 - text_width) / 2, 0), text, font=font)
 
-    # Draw the second text with a quote
     quote_font = ImageFont.truetype(FONT_PATH, int(FONT_SIZE/2))
     lines = textwrap.wrap(quote, width=int((1920 - 80*2)/quote_font.size))
     lines.reverse()
     y_text = 1080 - 100
     max_height = int(1080/2)
     total_height = 0
+
     for line in lines:
-        width, height = draw.textsize(line, quote_font)
+        text_bbox = draw.textbbox((0, 0), line, font=quote_font)
+        width = text_bbox[2] - text_bbox[0]
+        height = text_bbox[3] - text_bbox[1]
         if total_height + height > max_height:
             break
         total_height += height
@@ -78,25 +103,21 @@ while True:
     run_path = "/run/user"
 
     if not os.path.isdir(run_path):
-            os.makedirs(run_path)
-            try:
-                int(user_id)
-            except ValueError:
-                raise ValueError(f"{user_id} is not a valid user ID")
+        os.makedirs(run_path)
+        try:
+            int(user_id)
+        except ValueError:
+            raise ValueError(f"{user_id} is not a valid user ID")
 
-            path = f"{run_path}/{user_id}"
+        path = f"{run_path}/{user_id}"
 
-            if not os.path.isdir(path):
-                    os.makedirs(path)
+        if not os.path.isdir(path):
+            os.makedirs(path)
 
-                    print(path)
+            print(path)
 
-
-    # Save the modified photo
     photo.save(f'{run_path}/{user_id}/modified_photo.jpg')
-
-    # Set the modified photo as the wallpaper
     set_wallpaper(f'{run_path}/{user_id}/modified_photo.jpg')
 
-    # Sleep for 1 minute
-    time.sleep(60)
+    time.sleep(300)
+
